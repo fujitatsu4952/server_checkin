c2b4cae3234d72107ea0e43a2bd01ea6cb9bdbc1
fujitatsu4952 <fujitatsu49528@gmail.com>
2020-01-11 11:54:34 +0900

    SQLインジェクションを解決

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 // ルーティングするで
 var router = express.Router();
 const connection = require('../../app').connection
+var SqlString = require('sqlstring');
+function getEscapeString(input){
+  return SqlString.escape(`%${input}%`);
+};
 
 // routerにルーティングの動作を書いてく
 router.get('/', function (req, res) {
@@ -14,22 +18,25 @@ router.get('/', function (req, res) {
   });
 });
 
+
 router.post('/', (req, res) => {
+  console.log(getEscapeString(req.body.checkin_name_or_id));
+  console.log(req.body.checkin_name_or_id);
   connection.query(`
   select * from guest_mast 
     inner join hotel_mast
       on guest_mast.hotel_id = hotel_mast.hotel_id
-  where ((guest_mast.name LIKE "%${req.body.checkin_name_or_id}%") or (guest_mast.reservation_id LIKE "%${req.body.checkin_name_or_id}%")) 
+  where ((guest_mast.name LIKE ${getEscapeString(req.body.checkin_name_or_id)}) or (guest_mast.reservation_id LIKE ${getEscapeString(req.body.checkin_name_or_id)})) 
         and 
-        (hotel_mast.hotel_id = "${req.body.select_hotel}") 
+        (hotel_mast.hotel_id = '${req.body.select_hotel}') 
         and 
-        (guest_mast.checkin_date LIKE '%${req.body.checkin_date}%')`,
+        (guest_mast.checkin_date LIKE ${getEscapeString(req.body.checkin_date)})`,
     function (error, results, fields) {
       if (error) throw error;
       res.json(results);
     });
-
 });
+
 
 // ホテル情報を手に入れる
 router.get('/hotel', function (req, res) {
@@ -43,24 +50,24 @@ router.get('/hotel', function (req, res) {
 
 // ホテル情報の変更を受け取るAPI
 router.post('/update', (req, res) => {
-    connection.query(`
-    UPDATE hotel_mast
-    SET hotel_name = "${req.body.hotel_name}", hotel_pass = "${req.body.hotel_pass}"
-    WHERE hotel_id = "${req.body.hotel_id}"
-    ;`,
-    function (error, results, fields) {
-      if (error) throw error;
-    });
+  connection.query(`
+  UPDATE hotel_mast
+  SET hotel_name = ${SqlString.escape(req.body.hotel_name)}, hotel_pass = ${SqlString.escape(req.body.hotel_pass)}
+  WHERE hotel_id = "${req.body.hotel_id}"
+  ;`,
+  function (error, results, fields) {
+    if (error) throw error;
+  });
 });
 // 新規のホテル情報を受け取るAPI
 router.post('/insert', (req, res) => {
-    connection.query(`
-    insert into hotel_mast (hotel_name, hotel_id, hotel_pass)
-    VALUES ("${req.body.new_hotel_name}", "${req.body.new_hotel_id}", "${req.body.new_hotel_pass}")
-    ;`, 
-    function (error, results, fields) {
-      if (error) throw error;
-    });
+  connection.query(`
+  insert into hotel_mast (hotel_name, hotel_id, hotel_pass)
+  VALUES (${SqlString.escape(req.body.new_hotel_name)}, ${SqlString.escape(req.body.new_hotel_id)}, ${SqlString.escape(req.body.new_hotel_pass)})
+  ;`, 
+  function (error, results, fields) {
+    if (error) throw error;
+  });
 });
 
 
@@ -69,8 +76,8 @@ router.post('/checkin', (req, res) => {
   console.log(req.body)
   connection.query(`
   UPDATE guest_mast 
-  SET country = "${req.body.country}", name = "${req.body.name}", age = "${req.body.age}", address = "${req.body.address}", work = "${req.body.work}",work_type = "${req.body.work_type}", 
-      tell_number = "${req.body.tell}", sex = "${req.body.selected}" ,checkingtime = "${req.body.checkingtime}", status = "済"
+  SET country = "${req.body.country}", name = ${SqlString.escape(req.body.name)}, age = "${req.body.age}", address = ${SqlString.escape(req.body.address)}, work = "${req.body.work}",work_type = "${req.body.work_type}", 
+  tell_number = ${SqlString.escape(req.body.tell)}, sex = "${req.body.selected}" ,checkingtime = "${req.body.checkingtime}", status = "済"
   WHERE reservation_id = "${req.body.reserve}" 
         AND 
         hotel_id = "${req.body.hotel}";`, 
@@ -78,6 +85,7 @@ router.post('/checkin', (req, res) => {
     if (error) throw error;
   });
 });
+
 
 //routerをモジュールとして扱う準備
 module.exports = router;
